ff63c551b8e37fb540cbfdcdaf4efe89b9326f79
robin-whg <mail@robin-whg.com>
2022-02-21 21:27:28 +0100

    feat: change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
+    fontFamily: {
+      sans: ["Fira Sans", "sans-serif"],
+    },
     extend: {
       colors: {
         primary: "#FB923C",
