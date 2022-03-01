7dd9ca071e93a4051944fa4c768bfd84699708f5
robin-whg <mail@robin-whg.com>
2022-03-01 21:12:41 +0100

    feat: branding

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -15,7 +15,9 @@ const links: [string, string][] = [
     class="sticky top-0 flex w-full items-center justify-between border-b border-gray-300 bg-neutral-50/90 p-2 backdrop-blur dark:border-gray-600 dark:bg-neutral-900/90"
   >
     <div class="flex items-center gap-4">
-      <i-heroicons-outline-newspaper
+      <img
+        src="/src/assets/logo.svg"
+        alt="logo"
         class="h-[2.375rem] w-[2.375rem] flex-none text-primary"
       />
       <div
@@ -31,7 +33,25 @@ const links: [string, string][] = [
         >
       </div>
     </div>
-    <div>
+    <div class="flex gap-2">
+      <a href="https://github.com/robin-whg/hacker-news" target="_blank">
+        <base-button btn-type="tertiary" icon>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="feather feather-github h-6 w-6"
+          >
+            <path
+              d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+            ></path>
+          </svg>
+        </base-button>
+      </a>
       <base-button btn-type="tertiary" icon @click="toggleDark()">
         <i-heroicons-outline-moon v-if="isDark" class="h-6 w-6" />
         <i-heroicons-outline-sun v-else class="h-6 w-6" />
